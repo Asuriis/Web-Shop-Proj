fcb56a78112640ccc28cf5d06967d4acba493eab
Asuriis <nick.newsom10@gmail.com>
2024-12-10 23:08:34 +0200

    Cleaning
    Reduced image size and working on product sorting

--- Project/web_Shop_Proj.js
+++ Project/web_Shop_Proj.js
@@ -1,4 +1,4 @@
-const home = document.getElementById('navHome');
+/*const home = document.getElementById('navHome');
 const shop = document.getElementById('navShop');
 const deals = document.getElementById('navDeals');
 const basket = document.getElementById('navBasket');
@@ -30,35 +30,29 @@ const navBarElem = document.querySelectorAll('.navBar');
         navBar[i].classList.remove('active');
     }
     //navBar.classList.add('active');
-}*/
+}
 
 for (const navBar of navBarElem){
     navBar.addEventListener('click', function onSelect(event){
         navBar.classList.add('active');
         console.log(navBar.id);
     });
-}
-
+}*/
 
 fetch('https://fakestoreapi.com/users')
     .then(res=>res.json())
     .then(json=>console.log(json));
 
-
-/*async function getProductsFromServer(){
-    let responseObject = await fetch('https://fakestoreapi.com/products');
-    let storeProducts = await responseObject.json();
-    console.log(storeProducts);
-    showProducts(storeProducts);
-}*/
-
-fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(jsondata =>showProducts(jsondata))
+readData();
+async function readData() {
+    let myObject = await fetch('https://fakestoreapi.com/products');
+    let objectInfo = await myObject.json();
+    showProducts(objectInfo);
+}
 
 function showProducts(product){
     const table = document.getElementById('displayTable');
-    console.log(product);
+    //console.log(product);
     for (let i=0; i<product.length; i++){
         let productDetails = product[i];
         const row = table.insertRow();
@@ -66,8 +60,39 @@ function showProducts(product){
         row.insertCell().innerText = productDetails.description;
         row.insertCell().innerText = '$'+productDetails.price;
         let img = document.createElement('img');
-        img.setAttribute('style', 'width: 250px; height: 250px');
+        img.setAttribute('style', 'width: 150px; height: 150px');
         img.src = productDetails.image;
         row.insertCell().appendChild(img);
     }
 }
+
+/*let jeweleryBtn = document.getElementById('jewelery');
+jeweleryBtn.addEventListener('click', showLimitedProducts);
+
+readData();
+async function readData() {
+    let myObject = await fetch('https://fakestoreapi.com/products/category/jewelery');
+    let objectInfo = await myObject.json();
+    showLimitedProducts(objectInfo);
+}
+
+function showLimitedProducts(product){
+    const table = document.getElementById('displayTable');
+    table.innerHTML = '';
+    console.log(product);
+    for (let i=0; i<product.length; i++){
+        let productDetails = product[i];
+        const row = table.insertRow();
+        if(productDetails.category == jewelery){
+            console.log(product);
+            row.insertCell().innerText = productDetails.title;
+            row.insertCell().innerText = productDetails.description;
+            row.insertCell().innerText = '$'+productDetails.price;
+            let img = document.createElement('img');
+            img.setAttribute('style', 'width: 150px; height: 150px');
+            img.src = productDetails.image;
+            row.insertCell().appendChild(img);
+        }
+        else{''}
+    }
+}*/
